fe002fdae48903961c96609e999fa4d4f29782b0
Brandon Siegel <brandon.siegel@microsoft.com>
2018-06-25 13:34:18 -0700

    Initial prototype
    This app listens for GitHub "status" updates which are published when a
    PR's CI status is updated. When this occurs, it extracts the build ID
    from the status and queries the Travis API for job status. If a job
    defines a CHECK_NAME env variable, that job will be surfaced as a check
    and its status will be reported. Additionally, once a job completes if
    its log output contains a fenced `output` block, the contents of the
    block is assumed to be an [`output`
    object](https://developer.github.com/v3/checks/runs/#output-object), and
    will be attached to the check payload.

--- index.js
+++ index.js
@@ -1,10 +1,51 @@
+const Travis = require('./app/travis')
+const Store = require('./app/store')
+const Presenter = require('./app/presenter')
+
 module.exports = app => {
-  // Your code here
-  app.log('Yay, the app was loaded!')
+  app.on('status', async context => {
+    console.log(`Processing status update ${context.payload.id}`)
+    const travis = new Travis(context.payload)
+    if (!travis.initialized) {
+      console.log(`No Travis info detected in status update ${context.payload.id}`)
+      return
+    }
+    console.log(`Loaded Travis info for build ${travis.buildId}`)
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+    const jobs = await travis.getSupportedJobs()
+    console.log(`Discovered ${Object.keys(jobs).length} supported jobs`)
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+    const store = new Store()
+    const previous = store.updateJobs(travis.buildId, jobs)
+
+    const presenter = new Presenter(travis)
+    const diff = await presenter.calculateDiff(previous, jobs)
+    console.log(`Pending checks: ${diff.pending}`)
+    console.log(`Will create ${diff.create.length} checks and update ${diff.update.length} checks`)
+
+    const operations = []
+    operations.push(
+      diff.create.map(async c => {
+        const {jobId, payload} = c
+        console.log(`Creating check for job ${jobId}:\n${JSON.stringify(payload)}`)
+        const result = await context.github.checks.create(payload)
+        console.log(`Check ${result.data.id} created for job ${jobId}`)
+        store.setCheckRunId(travis.buildId, jobId, result.data.id)
+      })
+    )
+    operations.push(
+      diff.update.map(async c => {
+        const {payload} = c
+        console.log(`Updating check:\n${JSON.stringify(payload)}`)
+        await context.github.checks.update(payload)
+      })
+    )
+    await Promise.all(operations)
+
+    if (diff.pending === 0) {
+      console.log(`No remaining pending jobs, deleting job info from the store`)
+      store.delete(travis.buildId)
+    }
+    console.log(`Finished processing status update ${context.payload.id}`)
+  })
 }
