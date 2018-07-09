89a9b1a1790fee8b13438670552cd4b08f0a0efc
Brandon Siegel <brandon.siegel@microsoft.com>
2018-07-09 12:49:54 -0700

    Only process one Status event at a time per build

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,56 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 import { Status } from 'github-webhook-event-types'
-import { Application } from 'probot'
+import { Application, Context } from 'probot'
 
+import { BuildInfo } from './ci'
 import { GitHub } from './github'
 import { Travis } from './travis'
 
+const inProgress = new Set()
 let appId = parseInt(process.env.APP_ID || '', 10)
+
+async function getAppId (context: Context): Promise<number> {
+  if (!appId) {
+    appId = (await context.github.apps.get({})).data.id
+  }
+  return appId
+}
+
+async function processJobs (context: Context, buildInfo: BuildInfo): Promise<void> {
+  const travis = new Travis(context, buildInfo)
+  const jobs = await travis.getSupportedJobs()
+  if (!jobs) {
+    return
+  }
+  context.log(`Discovered ${jobs.length} supported jobs`)
+
+  const app = await getAppId(context)
+  const github = new GitHub(app, context, buildInfo, travis.getJobOutput.bind(travis))
+  const diff = await github.jobsToUpdate(jobs)
+  context.log(`Will create ${diff.create.length} checks and update ${diff.update.length} checks`)
+
+  const operations: Array<Promise<void>> = []
+  operations.push(
+    ...diff.create.map(async j => {
+      await github.createCheck(j)
+    })
+  )
+  operations.push(
+    ...diff.update.map(async j => {
+      await github.updateCheck(j)
+    })
+  )
+  await Promise.all(operations.map(p => p.catch(e => e)))
+}
+
+function getProgressKey (buildInfo: BuildInfo): string {
+  // Build ID is unique per Travis installation
+  return `${buildInfo.domain.toLowerCase()}/${buildInfo.id}`
+}
 
 export = (app: Application) => {
   app.on('status', async context => {
-    if (!appId) {
-      try {
-        appId = (await context.github.apps.get({})).data.id
-      } catch (e) {
-        context.log.error(e, `Failed to retrieve the App ID for this app`)
-        return
-      }
-    }
-
     context.log(`Processing status update ${context.payload.id}`)
     // tslint:disable-next-line:no-unnecessary-type-assertion
     const status = (context.payload as any) as Status
@@ -28,31 +60,27 @@ export = (app: Application) => {
       context.log(`No Travis info detected in status update ${context.payload.id}`)
       return
     }
-
     context.log(`Travis info detected in status update ${context.payload.id}`)
-    const travis = new Travis(context, buildInfo)
-    const jobs = await travis.getSupportedJobs()
-    if (!jobs) {
-      return
+
+    const key = getProgressKey(buildInfo)
+    if (!inProgress.has(key)) {
+      inProgress.add(key)
+      context.log(`Processing jobs for status update ${context.payload.id}`)
+      try {
+        await processJobs(context, buildInfo)
+      } catch (e) {
+        context.log.error(
+          e,
+          `Error occurred processing jobs for status update ${context.payload.id}`
+        )
+      } finally {
+        inProgress.delete(key)
+      }
+    } else {
+      context.log(
+        `Job processing already in progress for status update ${context.payload.id}, skipping...`
+      )
     }
-    context.log(`Discovered ${jobs.length} supported jobs`)
-
-    const github = new GitHub(appId, context, buildInfo, travis.getJobOutput.bind(travis))
-    const diff = await github.jobsToUpdate(jobs)
-    context.log(`Will create ${diff.create.length} checks and update ${diff.update.length} checks`)
-
-    const operations: Array<Promise<void>> = []
-    operations.push(
-      ...diff.create.map(async j => {
-        await github.createCheck(j)
-      })
-    )
-    operations.push(
-      ...diff.update.map(async j => {
-        await github.updateCheck(j)
-      })
-    )
-    await Promise.all(operations.map(p => p.catch(e => e)))
 
     context.log(`Finished processing status update ${context.payload.id}`)
   })
